8fea73ed72ed3b74b8cf2a3cb40ba030036dcbd3
Ben Beckers <ben.beckers@gmail.com>
2018-12-19 21:26:58 +0100

    Sock Merchant

---
+++ HackerRank.Tests/Warm-up Challenges/SockMerchant.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HackerRank.Tests.Warm_up_Challenges
+{
+    public class SockMerchant
+    {
+        [Fact]
+        public void SockMerchant_OnePair_ReturnOne()
+        {
+            //Arrange
+            HackerRank.Warm_up_Challenges.SockMerchant sockMerchant = new HackerRank.Warm_up_Challenges.SockMerchant();
+            List<int> socks = new List<int>()
+            {
+                1, 1
+            };
+
+            //Act
+            int result = sockMerchant.Result(socks.Count, socks.ToArray());
+
+            //Assert
+            Assert.True(result == 1);
+        }
+
+        [Fact]
+        public void SockMerchant_TwoPair_ReturnTwo()
+        {
+            //Arrange
+            HackerRank.Warm_up_Challenges.SockMerchant sockMerchant = new HackerRank.Warm_up_Challenges.SockMerchant();
+            List<int> socks = new List<int>()
+            {
+                1, 2, 1, 2
+            };
+
+            //Act
+            int result = sockMerchant.Result(socks.Count, socks.ToArray());
+
+            //Assert
+            Assert.True(result == 2);
+        }
+
+
+        [Fact]
+        public void SockMerchant_Unequal_ReturnTwoPair()
+        {
+            //Arrange
+            HackerRank.Warm_up_Challenges.SockMerchant sockMerchant = new HackerRank.Warm_up_Challenges.SockMerchant();
+            List<int> socks = new List<int>()
+            {
+                1, 2, 1, 2, 3
+            };
+
+            //Act
+            int result = sockMerchant.Result(socks.Count, socks.ToArray());
+
+            //Assert
+            Assert.True(result == 2);
+        }
+
+        [Fact]
+        public void SockMerchant_TestSample0()
+        {
+            //Arrange
+            HackerRank.Warm_up_Challenges.SockMerchant sockMerchant = new HackerRank.Warm_up_Challenges.SockMerchant();
+            List<int> socks = new List<int>()
+            {
+                10, 20, 20, 10, 10, 30, 50, 10, 20
+            };
+
+            //Act
+            int result = sockMerchant.Result(socks.Count, socks.ToArray());
+
+            //Assert
+            Assert.True(result == 3);
+        }
+
+        [Fact]
+        public void SockMerchant_TestSample1()
+        {
+            //Arrange
+            HackerRank.Warm_up_Challenges.SockMerchant sockMerchant = new HackerRank.Warm_up_Challenges.SockMerchant();
+            List<int> socks = new List<int>()
+            {
+                1, 1 ,3 ,1 ,2, 1, 3, 3, 3, 3
+            };
+
+            //Act
+            int result = sockMerchant.Result(socks.Count, socks.ToArray());
+
+            //Assert
+            Assert.True(result == 4);
+        }
+    }
+}
---
+++ HackerRank/Warm-up Challenges/SockMerchant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRank.Warm_up_Challenges
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/sock-merchant/problem
+    /// </summary>
+    public class SockMerchant
+    {
+        public int Result(int n, int[] ar)
+        {
+            int pairs = 0;
+
+            var socksPerType = ar.GroupBy(b => b).Select(b => new { Sock = b.Key, Count = b.Count() }).ToDictionary(b => b.Sock, b => b.Count);
+
+            foreach (var type in socksPerType)
+            {
+                if (type.Value % 2 != 0)
+                {
+                    int value = type.Value;
+                    pairs += --value / 2;
+                }
+                else
+                {
+                    pairs += type.Value / 2;
+                }
+            }
+
+            return pairs;
+        }
+    }
+}
