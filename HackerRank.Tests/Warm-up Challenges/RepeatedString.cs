e9387ad949b4460391e173a3792bb2cf8823bffb
Ben Beckers <ben.beckers@gmail.com>
2018-12-23 21:56:32 +0100

    Repeated Strings

---
+++ HackerRank.Tests/Warm-up Challenges/RepeatedString.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HackerRank.Tests.Warm_up_Challenges
+{
+    public class RepeatedString
+    {
+        [Fact]
+        public void Result_OneCharToTen_TenA()
+        {
+            //Arrange
+            HackerRank.Warm_up_Challenges.RepeatedString repeatedString = new HackerRank.Warm_up_Challenges.RepeatedString();
+
+            //Act
+            long result = repeatedString.Result("a", 10);
+
+            //Assert
+            Assert.Equal(10, result);
+        }
+
+        [Fact]
+        public void Result_TwoCharToTen_FiveA()
+        {
+            //Arrange
+            HackerRank.Warm_up_Challenges.RepeatedString repeatedString = new HackerRank.Warm_up_Challenges.RepeatedString();
+
+            //Act
+            long result = repeatedString.Result("ab", 10);
+
+            //Assert
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void Result_SixCharToTwo_OneA()
+        {
+            //Arrange
+            HackerRank.Warm_up_Challenges.RepeatedString repeatedString = new HackerRank.Warm_up_Challenges.RepeatedString();
+
+            //Act
+            long result = repeatedString.Result("abcdef", 2);
+
+            //Assert
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void Result_SixCharToTwo_ZeroA()
+        {
+            //Arrange
+            HackerRank.Warm_up_Challenges.RepeatedString repeatedString = new HackerRank.Warm_up_Challenges.RepeatedString();
+
+            //Act
+            long result = repeatedString.Result("bcdefa", 2);
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Result_SixToZero_OneA()
+        {
+            //Arrange
+            HackerRank.Warm_up_Challenges.RepeatedString repeatedString = new HackerRank.Warm_up_Challenges.RepeatedString();
+
+            //Act
+            long result = repeatedString.Result("abcdef", 0);
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Result_ThreeCharToTen_OneA()
+        {
+            //Arrange
+            HackerRank.Warm_up_Challenges.RepeatedString repeatedString = new HackerRank.Warm_up_Challenges.RepeatedString();
+
+            //Act
+            long result = repeatedString.Result("aab", 10);
+
+            //Assert
+            Assert.Equal(7, result);
+        }
+
+        [Fact]
+        public void Result_ZeroCharToTen_OneA()
+        {
+            //Arrange
+            HackerRank.Warm_up_Challenges.RepeatedString repeatedString = new HackerRank.Warm_up_Challenges.RepeatedString();
+
+            //Act
+            long result = repeatedString.Result("", 10);
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Result_StressTest()
+        {
+            //Arrange
+            HackerRank.Warm_up_Challenges.RepeatedString repeatedString = new HackerRank.Warm_up_Challenges.RepeatedString();
+
+            //Act
+            long result = repeatedString.Result("3N4UniiuXLtoMXVTOM0LK34mvjTQSQQeS6TRZ5bvOb8Wnaq8oJL5wo5NUbDVaa5todv8iBTd4cepF7gyKXrqJlbiLKLdkT7iqJ9Y", 10000000000000);
+
+            //Assert
+            Assert.True(result > 0);
+        }
+
+        [Fact]
+        public void Result_TestSample0()
+        {
+            //Arrange
+            HackerRank.Warm_up_Challenges.RepeatedString repeatedString = new HackerRank.Warm_up_Challenges.RepeatedString();
+
+            //Act
+            long result = repeatedString.Result("aba", 10);
+
+            //Assert
+            Assert.Equal(7, result);
+        }
+
+        [Fact]
+        public void ResultTestSample1()
+        {
+            //Arrange
+            HackerRank.Warm_up_Challenges.RepeatedString repeatedString = new HackerRank.Warm_up_Challenges.RepeatedString();
+
+            //Act
+            long result = repeatedString.Result("a", 1000000000000);
+
+            //Assert
+            Assert.Equal(1000000000000, result);
+        }
+    }
+}
---
+++ HackerRank/Warm-up Challenges/RepeatedString.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HackerRank.Warm_up_Challenges
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/repeated-string
+    /// </summary>
+    public class RepeatedString
+    {
+        // Complete the repeatedString function below.
+        public long Result(string s, long n)
+        {
+            #region Checks
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+
+            if (s.Count(b => b == 'a') == s.Length)
+            {
+                return n;
+            }
+
+            if (n == 0)
+            {
+                return 0;
+            }
+            #endregion
+
+            long countCharA;
+
+            long remainingChars = n % s.Length;
+            long fullDuplicates = n / s.Length;
+            countCharA = s.Count(b => b == 'a') * fullDuplicates;
+            countCharA += (s.Substring(0, (int)remainingChars)).Count(b => b == 'a');
+
+            return countCharA;
+        }
+    }
+}
