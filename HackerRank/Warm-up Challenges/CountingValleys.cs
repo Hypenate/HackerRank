b4bd19743ba720560d90e5e211b5863dd6a00a63
Ben Beckers <ben.beckers@gmail.com>
2018-12-23 19:11:19 +0100

    Change to comply with Codacy

--- HackerRank/Warm-up Challenges/CountingValleys.cs
+++ HackerRank/Warm-up Challenges/CountingValleys.cs
@@ -15,7 +15,7 @@ namespace HackerRank.Warm_up_Challenges
             int valleys = 0;
             HikerState hikerState;
 
-            foreach (var hill in s.ToUpper())
+            foreach (var hill in s.ToUpperInvariant())
             {
                 if (hill == 'D')
                 {
