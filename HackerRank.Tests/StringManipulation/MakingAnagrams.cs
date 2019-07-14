5d122bcd9e7d95235bec74e1a5d682d245cb18a0
Ben Beckers <ben.beckers@gmail.com>
2019-07-14 16:35:45 +0200

    Complete MakingAnagrams

---
+++ HackerRank.Tests/StringManipulation/MakingAnagrams.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HackerRank.Tests.StringManipulation
+{
+  public class MakingAnagrams
+  {
+    [Fact]
+    public void Result_SameInput()
+    {
+      //Arrange
+      HackerRank.StringManipulation.MakingAnagrams makingAnagrams = new HackerRank.StringManipulation.MakingAnagrams();
+      string a = "lepel";
+      string b = "lepel";
+
+      //Act
+      int result = makingAnagrams.Result(a, b);
+
+      //Assert
+      Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Result_DifferentInput()
+    {
+      //Arrange
+      HackerRank.StringManipulation.MakingAnagrams makingAnagrams = new HackerRank.StringManipulation.MakingAnagrams();
+      string a = "abc";
+      string b = "def";
+
+      //Act
+      int result = makingAnagrams.Result(a, b);
+
+      //Assert
+      Assert.Equal(6, result);
+    }
+
+    [Fact]
+    public void Result_Es()
+    {
+      //Arrange
+      HackerRank.StringManipulation.MakingAnagrams makingAnagrams = new HackerRank.StringManipulation.MakingAnagrams();
+      string a = "ee";
+      string b = "e";
+
+      //Act
+      int result = makingAnagrams.Result(a, b);
+
+      //Assert
+      Assert.Equal(1, result);
+    }
+
+    [Fact]
+    public void Result_TwoExtraEs()
+    {
+      //Arrange
+      HackerRank.StringManipulation.MakingAnagrams makingAnagrams = new HackerRank.StringManipulation.MakingAnagrams();
+      string a = "leepeel";
+      string b = "lepel";
+
+      //Act
+      int result = makingAnagrams.Result(a, b);
+
+      //Assert
+      Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void Result_TestSample0()
+    {
+      //Arrange
+      HackerRank.StringManipulation.MakingAnagrams makingAnagrams = new HackerRank.StringManipulation.MakingAnagrams();
+      string a = "cde";
+      string b = "abc";
+
+      //Act
+      int result = makingAnagrams.Result(a, b);
+
+      //Assert
+      Assert.Equal(4, result);
+    }
+
+    [Fact]
+    public void Result_TestSample1()
+    {
+      //Arrange
+      HackerRank.StringManipulation.MakingAnagrams makingAnagrams = new HackerRank.StringManipulation.MakingAnagrams();
+      string a = "fcrxzwscanmligyxyvym";
+      string b = "jxwtrhvujlmrpdoqbisbwhmgpmeoke";
+
+      //Act
+      int result = makingAnagrams.Result(a, b);
+
+      //Assert
+      Assert.Equal(30, result);
+    }
+
+    [Fact]
+    public void Result_TestSample2()
+    {
+      //Arrange
+      HackerRank.StringManipulation.MakingAnagrams makingAnagrams = new HackerRank.StringManipulation.MakingAnagrams();
+      string a = "showman";
+      string b = "woman";
+
+      //Act
+      int result = makingAnagrams.Result(a, b);
+
+      //Assert
+      Assert.Equal(2, result);
+    }
+  }
+}
---
+++ HackerRank/StringManipulation/MakingAnagrams.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace HackerRank.StringManipulation
+{
+  public class MakingAnagrams
+  {
+    public int Result(string a, string b)
+    {
+      int dropCount;
+      var aLetters = a.ToCharArray().OrderBy((l) => l).ToList();
+      var bLetters = b.ToCharArray().OrderBy((l) => l).ToList();
+
+      dropCount = ClearDifferenceInOccurances(aLetters, bLetters);
+
+      return dropCount;
+    }
+
+    public int ClearDifferenceInOccurances(List<char> a, List<char> b)
+    {
+      int dropCount = 0;
+
+      List<char> allLeters = new List<char>();
+      allLeters.AddRange(a);
+      allLeters.AddRange(b);
+
+      foreach (var letter in allLeters.Distinct())
+      {
+        var countA = a.Count(l => letter == l);
+        var countB = b.Count(l => letter == l);
+
+        // Check for duplicates or differences
+        if (countA != countB)
+        {
+          if (countA > countB)
+          {
+            for (int i = 0; i < (countA - countB); i++)
+            {
+              a.Remove(letter);
+              dropCount++;
+            }
+          }
+          else
+          {
+            for (int i = 0; i < (countB - countA); i++)
+            {
+              b.Remove(letter);
+              dropCount++;
+            }
+          }
+        }
+      }
+
+      return dropCount;
+    }
+  }
+}
