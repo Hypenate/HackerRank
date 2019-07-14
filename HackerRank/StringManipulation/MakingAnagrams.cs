734b2ff20922daf4dfd6688218b1393c3a1b5c57
Ben Beckers <ben.beckers@gmail.com>
2019-07-14 17:00:10 +0200

    Code cleanup

--- HackerRank/StringManipulation/MakingAnagrams.cs
+++ HackerRank/StringManipulation/MakingAnagrams.cs
@@ -11,15 +11,13 @@ namespace HackerRank.StringManipulation
     public int Result(string a, string b)
     {
       int dropCount;
-      var aLetters = a.ToCharArray().OrderBy((l) => l).ToList();
-      var bLetters = b.ToCharArray().OrderBy((l) => l).ToList();
 
-      dropCount = ClearDifferenceInOccurances(aLetters, bLetters);
+      dropCount = ClearDifferenceInOccurances(a.ToCharArray(), b.ToCharArray());
 
       return dropCount;
     }
 
-    public int ClearDifferenceInOccurances(List<char> a, List<char> b)
+    public int ClearDifferenceInOccurances(char[] a, char[] b)
     {
       int dropCount = 0;
 
@@ -32,25 +30,12 @@ namespace HackerRank.StringManipulation
         var countA = a.Count(l => letter == l);
         var countB = b.Count(l => letter == l);
 
-        // Check for duplicates or differences
+        // Check for differences
         if (countA != countB)
         {
-          if (countA > countB)
-          {
-            for (int i = 0; i < (countA - countB); i++)
-            {
-              a.Remove(letter);
-              dropCount++;
-            }
-          }
-          else
-          {
-            for (int i = 0; i < (countB - countA); i++)
-            {
-              b.Remove(letter);
-              dropCount++;
-            }
-          }
+          dropCount += (countA > countB)
+            ? (countA - countB)
+            : (countB - countA);
         }
       }
 
