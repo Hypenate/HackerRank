1327fafbf2ebf09bd333eae8ab55718873498771
Ben Beckers <ben.beckers@gmail.com>
2019-07-15 18:51:06 +0200

    Solve Alternating Characters

---
+++ HackerRank.Tests/StringManipulation/AlternatingCharacters.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+
+namespace HackerRank.Tests.StringManipulation
+{
+  public class AlternatingCharacters
+  {
+    [Theory]
+    [InlineData("AAAA", 3)]
+    [InlineData("BBBBB", 4)]
+    [InlineData("ABABABAB", 0)]
+    [InlineData("BABABA", 0)]
+    [InlineData("AAABBB", 4)]
+    public void Result_SampleInput(string providedSample, int providedResult)
+    {
+      //Arrange
+      HackerRank.StringManipulation.AlternatingCharacters alternatingCharacters = new HackerRank.StringManipulation.AlternatingCharacters();
+
+      //Act
+      int result = alternatingCharacters.Result(providedSample);
+
+      //Assert
+      Assert.Equal(providedResult, result);
+    }
+
+    [Theory]
+    [InlineData("AAABBBAABB", 6)]
+    [InlineData("AABBAABB", 4)]
+    [InlineData("ABABABAA", 1)]
+    public void Result_TestSample1(string providedSample, int providedResult)
+    {
+      //Arrange
+      HackerRank.StringManipulation.AlternatingCharacters alternatingCharacters = new HackerRank.StringManipulation.AlternatingCharacters();
+
+      //Act
+      int result = alternatingCharacters.Result(providedSample);
+
+      //Assert
+      Assert.Equal(providedResult, result);
+    }
+
+    [Theory]
+    [InlineData("ABBABBAA", 3)]
+    public void Result_TestSample2(string providedSample, int providedResult)
+    {
+      //Arrange
+      HackerRank.StringManipulation.AlternatingCharacters alternatingCharacters = new HackerRank.StringManipulation.AlternatingCharacters();
+
+      //Act
+      int result = alternatingCharacters.Result(providedSample);
+
+      //Assert
+      Assert.Equal(providedResult, result);
+    }
+  }
+}
---
+++ HackerRank/StringManipulation/AlternatingCharacters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRank.StringManipulation
+{
+  public class AlternatingCharacters
+  {
+    public int Result(string s)
+    {
+      char[] chars = s.ToCharArray();
+      var deletions = 0;
+
+      for (int i = 1; i < chars.Length; i++)
+      {
+        if (chars[i] == chars[i - 1]) deletions++;
+      }
+      
+      return deletions;
+    }
+  }
+}
